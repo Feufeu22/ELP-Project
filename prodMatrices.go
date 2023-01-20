eca7e25e717658d3f3ca7f801abf7b7a446d62b5
Thomas Feutren <thomas.feutren@insa-lyon.fr>
2023-01-20 13:39:50 +0100

    First success about a simple matrix product

--- prodMatrices.go
+++ prodMatrices.go
@@ -1,20 +1,36 @@
+/*-----------------------------------------------*/
+/*				 ** PROJET GO ** 		   		 */
+/*-----------------------------------------------*/
+
 package main
 
 import "fmt"
 
-// Création de la structure Matrice :
+/*-----------------------------------------------*/
+// ** STRUCTURES  ** //
+
+// La structure d'une Matrice :
 type Matrix struct {
-	Rows int
+	Rows    int
 	Columns int
-	Data [][]int
+	Data    [][]int
 }
 
-// Création de la fonction qui multiplie deux matrices
+// La structure d'un résultat de multiplication de matrices
+type MatrixResult struct {
+	result Matrix
+	err    error
+}
+
+/*-----------------------------------------------*/
+// ** FONCTIONS ** //
+
+// La fonction qui multiplie deux matrices
 func Multiply(matrix1 Matrix, matrix2 Matrix) (Matrix, error) {
 
-	// Vérifier si le produit est possible 
-    if matrix1.Columns != matrix2.Rows {
-		return Matrix{}, fmt.Errorf("Les matrices ne sont pas multipliable entre eux")
+	// Vérifier si le produit est possible
+	if matrix1.Columns != matrix2.Rows {
+		return Matrix{}, fmt.Errorf("Les matrices ne sont pas multipliable entre elles")
 	}
 
 	// Création de la matrice résultant
@@ -26,26 +42,75 @@ func Multiply(matrix1 Matrix, matrix2 Matrix) (Matrix, error) {
 
 	// Calcul de la matrice
 	for i := 0; i < matrix1.Rows; i++ {
-        for j := 0; j < matrix2.Columns; j++ {
-            var somme int
-            for k := 0; k < matrix1.Columns; k++ {
-                somme += matrix1.Data[i][k] * matrix2.Data[k][j]
-            }
-            result.Data[i][j] = somme
-        }
-    }
+		for j := 0; j < matrix2.Columns; j++ {
+			var somme int
+			for k := 0; k < matrix1.Columns; k++ {
+				somme += matrix1.Data[i][k] * matrix2.Data[k][j]
+			}
+			result.Data[i][j] = somme
+		}
+	}
 
-	// Afficher la matrice résultat 
-	return result,nil
+	// Afficher la matrice résultat
+	return result, nil
 
 }
 
+/*-----------------------------------------------*/
+// La fonction WORKER (effectue des multiplication en parallèle avec des goroutines)
+func worker(id int, jobs <-chan Matrix, results chan<- MatrixResult) {
+	for j := range jobs {
+		matrix1 := j
+		matrix2 := <-jobs
+		result, err := Multiply(matrix1, matrix2)
+		if err != nil {
+			results <- MatrixResult{result: result, err: err}
+		} else {
+			results <- MatrixResult{result: result, err: nil}
+		}
+	}
+}
+
+/*-----------------------------------------------*/
+// Fonction qui retourne les matrices à multiplier
+func getMatrix1() Matrix {
+	return Matrix{Rows: 2, Columns: 2, Data: [][]int{{1, 2}, {3, 4}}}
+}
+func getMatrix2() Matrix {
+	return Matrix{Rows: 2, Columns: 2, Data: [][]int{{3, 1}, {-1, 2}}}
+}
+
+/*-----------------------------------------------*/
+// ** PRINCIPAL ** //
+
 func main() {
 
-	// Créer une matrice
-	A := Matrix{Rows: 2, Columns: 2, Data: [][]int{{1, 2}, {3,4}}}
-	B := Matrix{Rows: 2, Columns: 2, Data: [][]int{{3,1}, {-1,2}}}
+	// Création des channels
+	jobs := make(chan Matrix)
+	results := make(chan MatrixResult)
 
-	// Afficher AxB
-    fmt.Println(Multiply(A, B))
+	// Création des workers
+	numWorkers := 4
+	for w := 1; w <= numWorkers; w++ {
+		go worker(w, jobs, results)
+	}
+
+	// Envoyer les matrices au channel jobs
+	numTasks := 1
+	for i := 0; i < numTasks; i++ {
+		matrix1 := getMatrix1()
+		matrix2 := getMatrix2()
+		jobs <- matrix1
+		jobs <- matrix2
+	}
+	close(jobs)
+	for i := 0; i < numTasks; i++ {
+		res := <-results
+		if res.err != nil {
+			fmt.Println(res.err)
+		} else {
+			fmt.Println(res.result)
+		}
+	}
+
 }
